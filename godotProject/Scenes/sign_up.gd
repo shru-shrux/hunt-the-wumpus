3a04f8f084d0948da09c892af872b2cbf08775fa
shru-shrux <shruti.kancherla@gmail.com>
2025-04-23 15:38:35 -0700

    trying to bring back login_new, made some sign in/log in changes

---
+++ godotProject/Scenes/sign_up.gd
@@ -0,0 +1,19 @@
+extends Panel
+
+signal createUser(user, password)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_sign_in_button_button_down() -> void:
+	createUser.emit($VBoxContainer/Username/TextEdit, $VBoxContainer/Password/TextEdit)
+
+func _on_cancel_button_2_button_down():
+	queue_free()
