59e2dcbecd7a32b033d42f798a70eeb1519e2bf3
shru-shrux <shruti.kancherla@gmail.com>
2025-04-27 11:59:49 -0700

    login/signup works more but still funky

--- godotProject/Scripts/sign_up.gd
+++ godotProject/Scripts/sign_up.gd
@@ -1,19 +1,31 @@
 extends Panel
 
-signal CreateUser(user, password)
+@onready var username_field = $VBoxContainer/Username/TextEdit
+@onready var password_field = $VBoxContainer/Password/TextEdit
+@onready var password_check_field = $VBoxContainer/Password/TextEdit
+@onready var error_label = $Error
+var login_manager = LoginManager
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	$SignInButton.pressed.connect(_on_sign_in_button_button_down)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_sign_in_button_button_down() -> void:
-	CreateUser.emit($VBoxContainer/Username/TextEdit, $VBoxContainer/Password/TextEdit)
+func _on_sign_in_button_button_down():
+	var user = username_field.text.strip_edges()
+	var password = password_field.text
+	var password2 = password_check_field.text
+	if password != password2:
+		error_label.text = "Passwords do not match."
+		return
+	if login_manager.signup(user, password):
+		error_label.text = "Account created! You are logged in."
+		# proceed to main menu
+	else:
+		error_label.text = "Username already taken."
 
 func _on_cancel_button_2_button_down():
-	queue_free()
+	get_parent().call_deferred("close_panels")
+	
+func _on_to_login_button_button_down():
+	# Tell parent OpeningScene to show login
+	get_parent().call_deferred("show_login")
+	visible = false
