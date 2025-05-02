62187af43911c46aee7cdc493bdf11e924171da4
Grace Liu <44937202+graceliu135@users.noreply.github.com>
2025-05-02 11:36:42 -0700

    set up leaderboard

--- godotProject/Scripts/Global.gd
+++ godotProject/Scripts/Global.gd
@@ -1,3 +1,18 @@
 extends Node
 
 var profile_start_tab : String = "profile"
+
+func _ready():
+	initialize_high_scores()
+
+func initialize_high_scores():
+	var highscore_path = "user://highscores.save"
+	
+	if not FileAccess.file_exists(highscore_path):
+		var default_scores = []
+		var file = FileAccess.open(highscore_path, FileAccess.WRITE)
+		file.store_var(default_scores)
+		file.close()
+		print("High score file created.")
+	else:
+		print("High score file already exists.")
