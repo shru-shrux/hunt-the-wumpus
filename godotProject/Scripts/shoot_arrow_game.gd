88adc75fc6a21b7d513ffa44ae74780e446c9cd6
Grace Liu <44937202+graceliu135@users.noreply.github.com>
2025-05-18 10:45:35 -0700

    most of arrow game finished

--- godotProject/Scripts/shoot_arrow_game.gd
+++ godotProject/Scripts/shoot_arrow_game.gd
@@ -12,7 +12,7 @@ var active = true
 
 func _ready():
 	slider.position.x = min_x
-	label.text = "Press SPACE when the slider is in the green."
+	label.text = "Shoot the Wumpus!\nPress SPACE when the slider is in the green."
 
 func _process(delta):
 	if active:
@@ -29,19 +29,42 @@ func _input(event):
 	if event is InputEventKey and event.pressed and event.keycode == KEY_SPACE:
 		if active:
 			_stop_game()
-		else:
-			_reset_game()
+		#else:
+			# put code here to continue the game
+			#_reset_game()
+
 
 func _stop_game():
 	active = false
 	var slider_center = slider.global_position.x + slider.size.x / 2
-	var screen_center = (min_x + max_x) / 2
-	var distance = abs(slider_center - screen_center)
-	var score = max(0, 100 - int(distance))  # Score out of 100
-	label.text = "Distance from center: %.1f px\nScore: %d/100\nPress SPACE to retry." % [distance, score]
+	
+	if slider_center >= 356 and slider_center < 438:
+		# red - 0 damage
+		label.text = "You missed! No damage was done to the Wumpus.\nPress SPACE to continue."
+	elif slider_center >= 438 and slider_center < 501:
+		# orange - 10% damage
+		label.text = "Slight damage was done to the Wumpus.\nPress SPACE to continue."
+	elif slider_center >= 501 and slider_center < 566:
+		# yellow - 25 % damage
+		label.text = "Medium damage was done to the Wumpus.\nPress SPACE to continue."
+	elif slider_center >= 566 and slider_center < 613:
+		# green - 50% damage
+		label.text = "Bullseye! A lot of damage was done to the Wumpus.\nPress SPACE to continue."
+	elif slider_center >= 613 and slider_center < 661:
+		label.text = "Medium damage was done to the Wumpus.\nPress SPACE to continue."
+	elif slider_center >= 661 and slider_center < 733:
+		label.text = "Slight damage was done to the Wumpus.\nPress SPACE to continue."
+	elif slider_center >= 733 and slider_center < 824:
+		label.text = "You missed! No damage was done to the Wumpus.\nPress SPACE to continue."
+	
+	
+	#var screen_center = (min_x + max_x) / 2
+	#var distance = abs(slider_center - screen_center)
+	#var score = max(0, 100 - int(distance))  # Score out of 100
+	#label.text = "Distance from center: %.1f px\nScore: %d/100\nPress SPACE to continue." % [distance, score]
 
-func _reset_game():
-	slider.position.x = min_x
-	direction = 1
-	active = true
-	label.text = "Press SPACE when the slider is in the green."
+#func _reset_game():
+	#slider.position.x = min_x
+	#direction = 1
+	#active = true
+	#label.text = "Press SPACE when the slider is in the green."
