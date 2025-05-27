4ddf77680ef9e5fecf490e65c4139fd36b01d6e3
Grace Liu <44937202+graceliu135@users.noreply.github.com>
2025-05-27 10:00:22 -0700

    main menu removed profile and changed to achievments

--- godotProject/Scripts/settings_tab_container.gd
+++ godotProject/Scripts/settings_tab_container.gd
@@ -33,10 +33,8 @@ const ACHIEVEMENT_THRESHOLDS = {
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if Global.profile_start_tab == "profile":
+	if Global.profile_start_tab == "options":
 		tab_container.current_tab = 2
-	elif Global.profile_start_tab == "options":
-		tab_container.current_tab = 3
 	elif Global.profile_start_tab == "leaderboard":
 		tab_container.current_tab = 0
 	elif Global.profile_start_tab == "achievements":
